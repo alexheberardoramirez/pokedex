e4ebb20d6e43c9dbcc5e73b19cfdffd103c1aff8
Alexramz <alexramz10@gmail.com>
2026-08-14 12:49:41 -0600

    First commit: API de Spring Boot for Pokedex

---
+++ src/test/java/com/alex/pokedex/PokedexApplicationTests.java
@@ -0,0 +1,15 @@
+package com.alex.pokedex;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class PokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
